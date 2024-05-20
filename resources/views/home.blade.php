ada8d7746ab2321164f74d1bb30616c8ac1dc25f
Sra Sontisirikit <sra.sontisirikit@gmail.com>
2024-05-20 18:45:35 +0700

    Add  Welcome text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">ระบบสมัครคอร์สเตโชวิปัสสนา</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    โปรดลงชื่อเข้าใช้งาน
                 </div>
             </div>
         </div>
