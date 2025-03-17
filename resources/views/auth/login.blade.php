1e8eff774b96d1f6acfe8a637bf16c35bb562ebf
Sra Sontisirikit <sra.sontisirikit@gmail.com>
2025-03-17 19:37:06 +0700

    Update Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,11 +8,12 @@
         <div class="row justify-content-center">
             <div class="col-md-12" id="course_button">
                 <div class="card">
-                    <a href="{{route('showCourseForStudent')}}" class="card-body text-center fs-5 bg-warning text-dark">
+                    <a href="{{route('showCourseForStudent')}}" class="card-body" style="text-align: center; font-size: 25px; background-color: #2d995b; color:black">
                         ดูตารางคอร์สปฏิบัติทั้งหมด
                     </a>
                 </div>
             </div>
+
 
             <div class="col-md-12" id="login_panel" style="padding-top: 20px;">
                 <div class="card">
