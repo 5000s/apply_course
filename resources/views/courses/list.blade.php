b5432875b490baefac295d2444ae89c2a5e6f689
Sra Sontisirikit <sra.sontisirikit@gmail.com>
2026-03-05 18:19:09 +0700

    Fix show error register in member course table page

--- resources/views/courses/list.blade.php
+++ resources/views/courses/list.blade.php
@@ -11,13 +11,16 @@
             </a>
         </div>
 
-        @if ($errors->has('course_id'))
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{ $errors->first('course_id') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        {{-- Display Validation Errors --}}
+        @if ($errors->any())
+            <div class="alert alert-danger mt-3">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
         @endif
-
         @php
             use Illuminate\Support\Str;
 
